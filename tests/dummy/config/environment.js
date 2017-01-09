8f9024f5b65743e25a8ef1dd3108768ad385ee0a
Spencer Price <spencer516@gmail.com>
2017-01-09 13:12:27 -0800

    get initial build to pass

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -17,6 +17,12 @@ module.exports = function(environment) {
       }
     },
 
+    browserify: {
+      transform: [
+        ['babelify', { presets: ['es2015'] }]
+      ]
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
